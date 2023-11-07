78ae773479be99c5e022ef7956c69b2d61b11995
Haciibrahimli <haci.ibrahimli@gmail.com>
2023-11-07 20:35:48 +0200

    loginpage

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from my_app.views import contact_view,login_view,products_view
+from my_app.views import contact_view,login_view,products_view,logout_view
 
 urlpatterns = [
 
     path('contact/',contact_view,name = 'contact'),
     path('login/',login_view,name = 'login'),
+    path('logout/',login_view,name = 'logout'),
     path('products/',products_view,name = 'products'),
 
 
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from my_app.models import *
 from my_app.forms import ContactForm
+from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def contact_view(request):
 
@@ -22,13 +24,6 @@ def contact_view(request):
     return render(request,'contact.html',context)
 
 
-
-def login_view(request):
-    context = {
-
-    }
-    return render(request,'login.html',context)
-
 def products_view(request):
 
     obj = Products.objects.all()
@@ -36,9 +31,37 @@ def products_view(request):
     if mycard is not None:
        obj = obj.filter(card_title__icontains = mycard)
 
+    paginator = Paginator(obj, 1)
+    page = request.GET.get('page', 1)
+    p = paginator.get_page(page)
+
+    try:
+        p = paginator.page(page)
+    except PageNotAnInteger:
+        p = paginator.page(1)
+    except EmptyPage:
+        p = paginator.page(paginator.num_pages)
+
     context = {
-        'obj':obj
+        'obj':obj,
+        'p':p
     }
     return render(request,'products.html',context)
 
 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("pwd")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("products/")
+        
+    context = {}
+
+    return render(request, "login.html", context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("products/")
